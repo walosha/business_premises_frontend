09e116250ff8fe41a4e21cb09cda17b2e724b335
Olawale Afuye <walosha@yahoo.com>
2022-02-24 10:10:53 +0100

    created user tab

---
+++ pages/admin/set-up/change_password.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+
+import Admin from "layouts/Admin.js";
+import { parseCookies, parseJwt } from "utils/parseCookie";
+import { Page } from "components/Helmet/Helmet";
+import withAuth from "lib/Hoc/withAuth";
+import useAllowedRoles from "lib/hooks/useRoles";
+
+function Settings() {
+	return (
+		<Page title={"Set Up"}>
+			<div className="flex flex-wrap">
+				<h1>Change Password</h1>
+			</div>
+		</Page>
+	);
+}
+
+function AdminSettings({ token }) {
+	useAllowedRoles(token);
+	return (
+		<Admin>
+			<Settings />
+		</Admin>
+	);
+}
+
+export async function getServerSideProps({ req }) {
+	const data = parseCookies(req);
+
+	return { props: { token: data.user || null } };
+}
+
+export default withAuth(AdminSettings);
