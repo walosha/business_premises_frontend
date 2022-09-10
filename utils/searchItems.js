f295be885cbc9ff4bae6bb053c92246f8941455f
olawale afuye <walosha@yahoo.com>
2022-09-10 16:25:37 +0100

    fix

--- utils/searchItems.js
+++ utils/searchItems.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 export function searchItems(route, search, page) {
-	const apiKey = "f9dfb1e8d466d36c27850bedd2047687";
-	return axios
-		.get(`/api/${route}?page=${page}&text=${search}`)
-		.then((r) => r.data.data)
-		.catch((error) => {
-			console.error(error);
-			return [];
-		});
+  return axios
+    .get(`/api/${route}?page=${page}&text=${search}`)
+    .then((r) => r.data.data)
+    .catch((error) => {
+      console.error(error);
+      return [];
+    });
 }
