b14d1a88f80a3b5406a8740ee6876a77a8cb80cc
olawale afuye <walosha@yahoo.com>
2022-09-10 16:19:11 +0100

    fixes

--- pages/api/payments/webhook.js
+++ pages/api/payments/webhook.js
@@ -8,66 +8,73 @@ require("lib/models/Businesses");
 require("lib/models/users");
 
 async function userHandler(req, res) {
-	const { method } = req;
+  const { method } = req;
 
-	switch (method) {
-		case "POST":
-			// Update or create data in your database
-			createPayment(req, res);
-			break;
+  switch (method) {
+    case "POST":
+      // Update or create data in your database
+      createPayment(req, res);
+      break;
 
-		default:
-			res.setHeader("Allow", ["POST"]);
-			res.status(405).end(`Method ${method} Not Allowed`);
-	}
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
 }
 
 async function createPayment(req, res) {
-	const {
-		InvoiceNumber,
-		Mac,
-		AmountPaid,
-		PaymentRef,
-		RequestReference,
-		...others
-	} = req.body;
-	const concatString =
-		InvoiceNumber + PaymentRef + Number(AmountPaid).toFixed(2);
-	console.log({ concatString });
+  const {
+    InvoiceNumber,
+    Mac,
+    AmountPaid,
+    PaymentRef,
+    RequestReference,
+    ...others
+  } = req.body;
+  const concatString =
+    InvoiceNumber + PaymentRef + Number(AmountPaid).toFixed(2);
 
-	const isvalid = generateHMAC256Auth(concatString) === Mac;
-	console.log({ isvalid });
-	if (isvalid) {
-		try {
-			const invoice = await Invoice.findOne({ InvoiceNumber });
-			if (invoice?.status == 1) {
-				return res.status(200).json({ message: "Duplicate record" });
-			}
+  const isvalid = generateHMAC256Auth(concatString) === Mac;
+  console.log({
+    concatString,
+    RequestReference,
+    Mac,
+    generateHMAC256Auth: generateHMAC256Auth(concatString),
+  });
 
-			if (invoice) {
-				await Payment.create({
-					InvoiceNumber,
-					AmountPaid,
-					...others,
-					PaymentRef,
-					Mac,
-					RequestReference,
-					invoice_id: invoice._id,
-				});
-			}
+  console.log({ isvalid });
 
-			await Invoice.findOneAndUpdate(
-				{ InvoiceNumber },
-				{ $set: { status: 1, paidSoFar: AmountPaid } },
-				{ new: true }
-			);
+  if (isvalid) {
+    try {
+      const invoice = await Invoice.findOne({ InvoiceNumber });
+      if (invoice?.status === 1) {
+        return res.status(200).json({ message: "Duplicate record" });
+      }
 
-			return res.status(200).json({});
-		} catch (error) {
-			return res.status(500).json({ success: false, data: error.message });
-		}
-	}
-	return res.status(401).json({ success: false, data: "Invalid" });
+      if (invoice) {
+        await Payment.create({
+          InvoiceNumber,
+          AmountPaid,
+          ...others,
+          PaymentRef,
+          Mac,
+          RequestReference,
+          invoice_id: invoice._id,
+        });
+      }
+
+      await Invoice.findOneAndUpdate(
+        { InvoiceNumber },
+        { $set: { status: 1, paidSoFar: AmountPaid } },
+        { new: true }
+      );
+
+      return res.status(200).json({});
+    } catch (error) {
+      return res.status(500).json({ success: false, data: error.message });
+    }
+  }
+  return res.status(401).json({ success: false, data: "Invalid" });
 }
 
 module.exports = withSentry(connectDB(allowCors(userHandler)));
