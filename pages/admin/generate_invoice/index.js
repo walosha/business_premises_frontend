2c0c2661967d83a1572a6f62f86f6ebfee14e6a3
olawale afuye <walosha@yahoo.com>
2022-09-20 09:31:44 +0100

    fix

--- pages/admin/generate_invoice/index.js
+++ pages/admin/generate_invoice/index.js
@@ -1,11 +1,4 @@
 import React from "react";
-
-// components
-
-// import CardTable from "components/Cards/CardTable.js";
-
-// layout for page
-
 import Admin from "layouts/Admin.js";
 import withAuth from "lib/Hoc/withAuth";
 import { Page } from "components/Helmet/Helmet";
@@ -18,9 +11,6 @@ function Tables() {
         <div className="w-full lg:w-12/12 px-4">
           <GenerateBill />
         </div>
-        {/* <div className="w-full lg:w-4/12 px-4">
-          <CardProfile />
-        </div> */}
       </div>
     </Page>
   );
