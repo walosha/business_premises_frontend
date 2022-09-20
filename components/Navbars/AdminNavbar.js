f6a4ef8257e7b747c28c2f1ac5db63980ef47430
olawale afuye <walosha@yahoo.com>
2022-09-20 09:22:37 +0100

    fixed

--- components/Navbars/AdminNavbar.js
+++ components/Navbars/AdminNavbar.js
@@ -4,41 +4,39 @@ import UserDropdown from "components/Dropdowns/UserDropdown.js";
 import axios from "axios";
 
 export default function Navbar() {
-	const [user, setUser] = useState(0);
-	useEffect(() => {
-		axios
-			.get("/api/users/me")
-			.then((response) => {
-				setUser(response.data.data);
-			})
-			.catch(console.log);
-	}, []);
-	return (
-		<>
-			{/* Navbar */}
-			<nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
-				<div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
-					{/* Brand */}
-					<a
-						className="text-white text-sm uppercase hidden lg:inline-block mr-4 font-semibold"
-						href="#"
-						onClick={(e) => e.preventDefault()}
-					>
-						Dashboard
-					</a>
+  const [user, setUser] = useState(0);
+  useEffect(() => {
+    axios
+      .get("/api/users/me")
+      .then((response) => {
+        setUser(response.data.data);
+      })
+      .catch(console.log);
+  }, []);
+  return (
+    <>
+      {/* Navbar */}
+      <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
+        <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
+          {/* Brand */}
+          <a
+            className="text-white text-sm uppercase hidden lg:inline-block mr-4 font-semibold"
+            href="/#"
+            onClick={(e) => e.preventDefault()}
+          >
+            Dashboard
+          </a>
 
-					<p className="text-white text-sm uppercase hidden lg:inline-block font-semibold">
-						Welcome, {user.name}
-					</p>
-					{/* Form */}
-					<form className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3"></form>
-					{/* User */}
-					<ul className="flex-col md:flex-row list-none items-center hidden md:flex">
-						<UserDropdown />
-					</ul>
-				</div>
-			</nav>
-			{/* End Navbar */}
-		</>
-	);
+          <p className="text-white text-sm uppercase hidden lg:inline-block font-semibold">
+            Welcome, {user.name}
+          </p>
+
+          <ul className="flex-col md:flex-row list-none items-center hidden md:flex">
+            <UserDropdown />
+          </ul>
+        </div>
+      </nav>
+      {/* End Navbar */}
+    </>
+  );
 }
